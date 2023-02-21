2d271cac2704a59b82b6f55caa2f7f1a7370631e
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-02-21 04:23:44 -0800

    modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,16 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar(' ');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
