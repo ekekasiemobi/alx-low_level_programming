2f03aa649edf3b39a4dad144763bca79da0f1699
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-02-21 04:38:13 -0800

    created main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  * main - prints the word _putchar
  * prints a new line
- * Return: always 0 (success)
+ * Return: always return 0 
  */
 
 int main(void)
 {
-	putchar(' ');
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
