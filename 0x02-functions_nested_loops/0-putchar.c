17803d12f1f2c8a31deb3a9e5096079facde2a27
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-02-21 04:13:00 -0800

    created 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the word _putchar
+ * prints a new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
