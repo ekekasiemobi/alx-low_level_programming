8228e9366435d6b4e4ae0942ccba07fe683e3ae5
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-02 04:20:22 -0800

    created 2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - copies strings
+ * @dest: destiction
+ * @src: source
+ * @n: space limit
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
