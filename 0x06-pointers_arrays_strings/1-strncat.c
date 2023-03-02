37108da086951797ad922b95c8e2d0f9ced10ffb
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-02 03:58:21 -0800

    created 1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,27 @@
 #include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: destinction
+ * @src: source
+ * @n: space limit
+ * Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	return (result)
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
