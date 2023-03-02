0af20bdd8b1aeb77bafa489bbac9d5709de1e618
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-02 04:28:47 -0800

    created 2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
+	
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
