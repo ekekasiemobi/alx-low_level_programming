8c7345a9d4f5e5476cd6a290eb7c3c3a08360c27
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-08 05:17:21 -0800

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,15 +8,16 @@
 
 void _print_rev_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                return;
-        }
+	if (*s == '\0')
+	{
+		return;
+	{
 
-        else
-        {
-                _print_rev_recursion(s + 1);
+	else
+
+	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-        }
+	}
 }
 
