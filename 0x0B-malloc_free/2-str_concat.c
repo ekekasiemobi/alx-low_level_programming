5dd086b08d06da9865d9991fe394bd2470c3c130
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-14 06:03:31 -0700

    task3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,14 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL )
 	{
-		return (" ");
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	concat_str = (char *) malloc(strlen(s1) + strlen(s2) + 1);
