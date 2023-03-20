4c83cc34870d932953fead94e620ae1f80e40335
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-20 09:08:09 -0700

    added main.c

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,5 +1,5 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "1-pi.h"
 #include <stdio.h>
 
 /**
@@ -9,9 +9,11 @@
  */
 int main(void)
 {
-    int s;
+    float a;
+    float r;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
     return (0);
 }
