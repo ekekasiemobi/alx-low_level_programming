da72bcd370a92d56e877a9c068a1813790471b65
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-04-05 19:55:20 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t linked list
+ * @head: pointer to the head of the linked list
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	else (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+}
