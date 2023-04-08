962ef97bdc6c445e4ccc8baf67a9999dbfa07271
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-04-08 06:04:14 -0700

    task 3 working perfectly

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit of the the index
+ * @n:  this is the bit
+ * @index: where the bit is taken
+ * Return: return -1 unsuccesful, successful return 1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	else
+	{
+		(*n |= 1 << index);
+
+		return (1);
+	}
+}
