287bfd1887231d2e3d89aab59ed51f3550ec4bae
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-04-10 02:43:57 -0700

    having a little issue with the task3 output

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
 * Return: 1 on success, -1 on failure
 */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int i, file_open, file_write;
 
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content);
 		return (-1);
 	}
 
-	file_open = open(filename, O_WRONLY | 0_APPEND);
+	file_open = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_open == -1)
 	{
