c5d25d70a479137a2f88be60bb46af31661e84a8
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-04-10 12:46:49 -0700

    solved betty issues for task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_bit - gets the bit of the the index
- * @n:  this is the bit
- * @index: where the bit is taken
- * Return: return -1 unsuccesful, successful return 1
+ * set_bit - gets the bit of the index
+ * @n:  this is the bit to be set to 1
+ * @index: this is where the bit is taken
+ * Return: return -1 when it fails, return 1 when its successful
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
