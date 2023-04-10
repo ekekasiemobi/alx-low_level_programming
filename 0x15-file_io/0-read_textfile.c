21a78d6cd3a5e8e105c28993e394b2a3c4177dab
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-04-09 23:50:32 -0700

    task0 is printing correct output but there is still error with betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,58 @@
- ssize_t file_open, file_read, file_write;
-        char *buffer;
+#include "main.h"
+/**
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file_open, file_read, file_write;
+	char *buffer;
 
-        if (filename == NULL)
-        {
-                return (0);
-        }
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 
         if (buffer == NULL)
         {
-                return (0)
-                free(buffer)
+                return (0);
+                free(buffer);
         }
 
         file_open = open(filename,O_RDONLY);
+	 file_read = read(file_open, buffer, letters);
+	 file_write = write(STDOUT_FILENO, buffer, file_read);
+
+
 
         if (file_open == -1)
         {
-                free(buffer)
-                return (0)
-        }
+                free(buffer);
+                return (0);
+	} 
 
-        file_read = read(file_open, buffer, letter);
 
-        else if (file_read == -1)
-        {
-                free(buffer)
-                return (0)
-        }
-
-        file_write = write(STDOUT_FILENO, buffer, file_read)
+        	else if (file_read == -1)
+        	{
+                free(buffer);
+                return (0);
+        	}
 
         else if (file_write == -1)
         {
-                free(buffer)
+                free(buffer);
+		return (0);
+	}
 
+	else 
+	{
+		free(buffer);
+		close(file_open);
+	}
+
+
+	return (file_write);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 
