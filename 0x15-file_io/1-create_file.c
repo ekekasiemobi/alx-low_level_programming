791584b332f82a27f75ed49ca01d80e5c2bfd8d2
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-04-10 19:34:51 -0700

    cant seem to fix betty error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (text_content != NULL)
+
 	{
 		for (i = 0; text_content[i]; i++);
 
@@ -33,12 +34,6 @@ int create_file(const char *filename, char *text_content)
 		{
 			close(file_open);
 			return (-1);
-		}
-
-		else
-		{
-			close(file_open);
-			return (1);
 		}
 	}
 
