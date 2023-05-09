59f761e0b513cdd96d24c7ccad08d8bd4e903f8c
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-05-08 19:09:31 -0700

    modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,23 +24,30 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (text_content != NULL)
+
 	{
+		for (i = 0; text_content[i]; i++);
 
-		for (i = 0; text_content[i]; i++)
+		file_write = write(file_open, text_content, i);
+
+		if (file_write == -1)
 		{
+			close(file_open);
+			return (-1);
+		}
 
-			file_write = write(file_open, text_content, i);
-
-			if (file_write == -1)
-			{
-				close(file_open);
-				return (-1);
-			}
+		else
+		{
+			close(file_open);
+			return (1);
 		}
 	}
 
-	close(file_open);
-	return (1);
+	else
+	{
+		close(file_open);
+		return (-1);
+	}
 
 }
 
