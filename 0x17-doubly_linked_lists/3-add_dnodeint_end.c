698049d782d9a408a0a2493b031a9c68c0cf4fb7
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-16 02:45:22 -0700

    fixed trailing whiteespaes

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,19 +9,19 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *temp;
-	
+i	dlistint_t *temp;
+
 	temp = *head;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-    
+
 	new_node->n = n;
 	new_node->next = NULL;
-    
+
 	if (*head == NULL)
 	{
 		new_node->prev = NULL;
@@ -33,11 +33,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		{
 			temp = temp->next;
 		}
-        
+
 		temp->next = new_node;
 		new_node->prev = temp;
 	}
 
-	return new_node;
+	return (new_node);
 }
 
