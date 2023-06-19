19f7a9cafd1b4aa226578c936007a06c9116b98f
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-19 04:38:35 -0700

    dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+/**
+ * mod - checks if an integer a is divisible by an integer b
+ * @a: integer
+ * @b: integer
+ * Return: a % b
+ */
+int div(int a, int b) {
+    return a / b;
+}
+
+int mod(int a, int b) {
+    return a % b;
+}
+
