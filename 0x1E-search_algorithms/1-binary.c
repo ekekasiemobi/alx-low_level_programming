7bab7c09e7c57efa6d6abe986cc44e7cf192d459
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-10 01:55:58 -0700

    searches for a value in a sorted array of integers

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches to find value in array and return the index
+ * @array: array to search for the value
+ * @size: size of array searched from
+ * @value: value to search for in the array
+ * Return: index of value or -1 if thr value not found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, left, right, mid;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
