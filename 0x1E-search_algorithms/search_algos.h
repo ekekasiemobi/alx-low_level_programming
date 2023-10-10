241212e10284c103ba4fb6504c188f20d960a958
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-10 01:18:38 -0700

    creates the header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_H */
